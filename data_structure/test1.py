7ae4bcc6fdeb5d50b4fbfeb1eed769880c66ee27
SHRP12 <architect117711@gmail.com>
2025-08-20 14:06:01 +0300

    added locate card function

--- data_structure/test1.py
+++ data_structure/test1.py
@@ -6,5 +6,12 @@ def locate_card(cards,query):
             return position
         position += 1
 
-    return f"Not founded"
+    return -1
     
+cards = [1,2,3,4,5,6,7,8,9]   
+query = 6
+result = locate_card(cards,query)
+if result != -1:
+    print(f"Card position is = {result + 1}")
+else:
+    print("Card not founded")
