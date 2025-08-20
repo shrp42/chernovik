9a555c76c9d6ffc52ac96049826c3d2d11c2e581
SHRP12 <architect117711@gmail.com>
2025-08-20 13:54:51 +0300

    New file added

---
+++ data_structure/test1.py
@@ -0,0 +1,10 @@
+def locate_card(cards,query):
+    position = 0 
+
+    while position < len(cards):
+        if cards[position] == query:
+            return position
+        position += 1
+
+    return f"Not founded"
+    
