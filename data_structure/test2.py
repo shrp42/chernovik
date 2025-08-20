992d1f269248966540cee787a60b8a04712f703e
SHRP12 <architect117711@gmail.com>
2025-08-20 21:12:10 +0300

    kfjkdjfs

--- data_structure/test2.py
+++ data_structure/test2.py
@@ -1,1 +1,29 @@
 # Бинарный поиск - подходит для работы с большими базами данных
+
+def test_location(cards,query,mid):
+    if cards[mid] == query:
+        if mid-1 >= 0 and cards[mid-1] == query:
+            return "Left"
+        else:
+            return "Founded"
+    elif cards[mid] < query:
+        return "Left"
+    else:
+        return "Right"
+    
+def locate_card(cards,query):
+    lo, hi = 0, len(cards)-1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = test_location(cards,query,mid)
+        if result == "Founded":
+            return mid
+        elif result == "Left":
+            hi = mid - 1 
+        elif result == "Right":
+            lo = mid + 1
+    return -1
+
+cards = [1,2,3,4,5,6,7,8,9,10]
+query = 2
+locate_card(cards,query)        
