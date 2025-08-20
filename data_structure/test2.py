977e98f3da8facc4c53e063bf53556bdd4cde266
SHRP12 <architect117711@gmail.com>
2025-08-20 21:14:17 +0300

    ds

--- data_structure/test2.py
+++ data_structure/test2.py
@@ -24,6 +24,6 @@ def locate_card(cards,query):
             lo = mid + 1
     return -1
 
-cards = [1,2,3,4,5,6,7,8,9,10]
+cards = [10,9,8,7,6,5,4,3,2,1]
 query = 2
-locate_card(cards,query)        
+locate_card(cards,query)  
