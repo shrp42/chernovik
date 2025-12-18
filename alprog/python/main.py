76bc69379174b9de50f4ccce828425a26d9cca62
SHRP12 <architect117711@gmail.com>
2025-12-18 20:36:54 +0300

    start point

--- alprog/python/main.py
+++ alprog/python/main.py
@@ -5,3 +5,13 @@ def main ():
 if __name__ == "__main__":
     main()
 
+
+def bunch_of_smt():
+
+    word = str("word")
+    num = int(12)
+    float_num = float(4.2)
+    bool_smt = True
+    print(word,num,float_num,bool_smt)
+
+bunch_of_smt()
