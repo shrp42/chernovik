cddfd1ef1b11216a2e49b4510d2db803534eb621
SHRP12 <architect117711@gmail.com>
2025-12-22 14:58:48 +0300

    DONE CHERNOVIK LESSON1

--- alprog/python/chernovik.py
+++ alprog/python/chernovik.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import *
+import time
 def squared_list(t: List[int]) -> List[int]:
     result = []
     for x in t:
@@ -7,4 +8,25 @@ def squared_list(t: List[int]) -> List[int]:
 
 nums = [1,2,3,4,5,6,7,8,9]
 result = squared_list(nums)
-result()
+
+
+
+def divided_by() -> List[int]:
+    for x in range(1,100):
+        if x % 5 == 0 and x % 2 == 0:
+            print(x)
+
+
+# def countdown(start: int = 10, end: int = 1, message: Optional[str] = "BOOM" * 100, delay = 0.5) -> None:
+#     current = start
+#     while current >= end:
+#         print(current)
+#         time.sleep(delay)
+#         current -= 1
+#     print(message)
+#
+# countdown()
+#
+#
+#
+def
