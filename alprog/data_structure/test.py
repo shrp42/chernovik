aa923c7e5dfecdfa46d373da99f0d21506988b41
SHRP12 <architect117711@gmail.com>
2026-01-09 00:46:50 +0300

    ADD SOME DEFS IN TEST

--- alprog/data_structure/test.py
+++ alprog/data_structure/test.py
@@ -67,3 +67,77 @@ def max_subarray(nums: list[int]) -> int:
 
 # nums = [12,2,4,6,-100,14,13,-2,4,6,-7]
 # print(max_subarray(nums))
+
+
+
+def two_s(nums: list[int], target: int) -> int:
+    if not nums:
+        return 0
+
+    seen = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], i]
+        seen[num] = i
+
+# nums = [1,2,3,4,5,6,7,8,9]
+# print(two_s(nums,9))
+
+
+
+def del_d(nums: list[int]) -> int:
+    if not nums:
+        return 0
+
+    i = 0
+
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1
+
+# nums = [1,1,2,42,42,42,52,52,52,654,343,21,12]
+# k = del_d(nums)
+# print(k, nums[:k])  # 2 [1,2]
+
+
+def rot(nums: list[int], k: int) -> int:
+    n = len(nums)
+    if n == 0:
+        return 0
+
+    k = k % n
+
+    def rev(left, right):
+       while left < right:
+           nums[left], nums[right] = nums[right], nums[left]
+           left += 1
+           right -= 1
+
+    rev(0, n - 1)
+    rev(0, k - 1)
+    rev(k, n - 1)
+
+# nums = [10,20,30,40,50,60,70,80]
+# rot(nums, 3)
+# print(nums)
+
+
+def max_s(nums: list[int]):
+    if not nums:
+        return 0
+
+    cur_nums = nums[0]
+    max_n = nums[0]
+
+    for num in nums[1:]:
+        cur_nums = max(num, cur_nums + num)
+        max_n = max(max_n, cur_nums)
+
+    return max_n
+
+nums = [-1,-2,4,53,-35,3]
+print(max_s(nums))
